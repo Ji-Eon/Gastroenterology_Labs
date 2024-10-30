6c53304338317924abae19b8e36cb0840c9ab23c
BADAGREEN\kakas <medikim3551@gmail.com>
2024-10-30 22:42:31 +0900

    rebuild

--- gastoenterology_labs/.next/server/server-reference-manifest.js
+++ gastoenterology_labs/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"6oq28P4upQENR9kcdbg+FM1gB1SOzGr9LIvx9l1Q+lQ=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
