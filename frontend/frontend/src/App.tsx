5c9af977d232a105d203f434a38621fd2e65ca67
JiEon <medikim355@gmail.com>
2024-11-01 00:05:33 +0900

    recact vite project

---
+++ frontend/frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <p>124ssssadadadwwws3</p>
+      <p>124ssssadadadwwws3</p>
+    </div>
+  )
+}
+
+export default App
