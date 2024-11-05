2cea237209d315d0abd59d6c8555222f6e0f38a1
JiEon <medikim355@gmail.com>
2024-11-05 22:31:42 +0900

    delpoy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+  base: '/Gastroenterology_Labs/', // repository-name에 자신의 리포지토리 이름을 입력하세요
+  plugins: [react()]
 });
